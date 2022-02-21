04d7469489e8c945370fd7ae0546a94eabd9ca53
Sukriti Srivastava <79142553+sukscode@users.noreply.github.com>
2022-02-21 10:38:50 +0530

    part 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import ReactDOM  from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import example from "./example";
 
 function App(){
     return (
         <div>
           <Header />
-          <Note />
+          {example.map(noteItem=>(<Note key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content} />
+          ))}
           <Footer />
         </div>
       );
